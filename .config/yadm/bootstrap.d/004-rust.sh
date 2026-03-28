891f260c11ed5c9c1c50252ec75c10f5dd163d13
Dj Isaac <contact@dejaydev.com>
2026-03-28 01:49:52 -0700

    fix: some holes in the workflow (#3)

--- .config/yadm/bootstrap.d/004-rust.sh
+++ .config/yadm/bootstrap.d/004-rust.sh
@@ -3,8 +3,11 @@
 echo "[.files] - Installing rust via rustup"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+# shellcheck source=/dev/null
+. "$HOME/.cargo/env"
+
 echo "[.files] - Installing rust packages (1/2)"
 cargo install cargo-binstall
 
 echo "[.files] - Installing rust packages (2/2)"
-cargo binstall -y fnm hunt topgrade tealdeer zoxide
+cargo binstall -y fnm hunt ripgrep topgrade tealdeer zoxide
