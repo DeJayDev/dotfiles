8c8f240a9677d78971d82d3a29cbf4c05cba975e
Dj Isaac <contact@dejaydev.com>
2026-07-13 11:53:53 -0700

    fix: change remote uri

--- .config/yadm/bootstrap.d/001-yadm.sh
+++ .config/yadm/bootstrap.d/001-yadm.sh
@@ -4,7 +4,7 @@ cd "$HOME"
 
 if [ -f "$HOME/.ssh/id_ed25519" ] || [ -f "$HOME/.ssh/id_rsa" ]; then
   echo "[.files] - Updating dotfiles to use ssh"
-  yadm remote set-url origin "git@github.com:dejaydev/dotfiles.git"
+  yadm remote set-url origin "git@github.com:DeJayDev/dotfiles.git"
 else
   echo "[.files] - WARNING: No SSH key found, skipping SSH remote switch"
   echo "[.files] - Run 'ssh-keygen -t ed25519' and add key to GitHub, then re-run this script"
